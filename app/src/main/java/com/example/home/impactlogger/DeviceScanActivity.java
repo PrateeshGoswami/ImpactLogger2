ed6fea4b892799512e3e010ba3b441a97c9ff6f7
Prateesh Goswami <prateeshgoswami@gmail.com>
2016-07-05 17:34:01 -0700

    Feat: Add google play service Admobile

--- app/src/main/java/com/example/home/impactlogger/DeviceScanActivity.java
+++ app/src/main/java/com/example/home/impactlogger/DeviceScanActivity.java
@@ -24,6 +24,9 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -46,6 +49,16 @@ public class DeviceScanActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView = (ListView) findViewById(R.id.listview_devices);
+
+//        Adbanner
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
 
 //        getActionBar().setTitle(R.string.title_devices);
         mHandler = new Handler();
